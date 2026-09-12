209dabc42a54fb5b805796222a51d38df0f61d94
Mohd Tasawwur Alam <mohdtasawwuralam>
2026-09-12 16:52:58 +0530

    Added student calculator

---
+++ student_calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    float phy, chem, math;
+    float total, percentage, average;
+    printf("Enter PHysics marks (out of 100)L ");
+    scanf("%f", &phy);
+    printf("Enter Chemistry marks (out of 100): ");
+    scanf("%f", &chem);
+    printf("Enter Mathematics marks (out of 100): ");
+    scanf("%f", &math);
+    total = phy + chem + math;
+    average = total / 3.0;
+    percentage = (total / 300.0) * 100;
+    printf("Total marks: %.2f\n", total);
+    printf("Average marks: %.2f\n", average);
+    printf("Percentage: %.2f%%\n", percentage);
+    return 0;
+}
